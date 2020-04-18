d3ec452edd2682fef4ada2da4267cfbea8e04fec
brentvollebregt <brentvollebregt@users.noreply.github.com>
2020-04-18 03:41:52 +0000

    deploy: a667097918b76d3618a04c103163a7ebecd57887

---
+++ precache-manifest.82924b8a9a70560ecca4a1731ff1fd64.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "797f0a0734f8576e2a71eec59a025df6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aae3255c410dadefe1ad",
+    "url": "/static/css/main.d40c91df.chunk.css"
+  },
+  {
+    "revision": "32eca20fd28881d62ea8",
+    "url": "/static/js/2.13e615f8.chunk.js"
+  },
+  {
+    "revision": "445348ff3b1bc46a5befc2e9edb07aa2",
+    "url": "/static/js/2.13e615f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aae3255c410dadefe1ad",
+    "url": "/static/js/main.d8561673.chunk.js"
+  },
+  {
+    "revision": "f894e40473f9fda0cea6",
+    "url": "/static/js/runtime-main.fc00fddc.js"
+  },
+  {
+    "revision": "1c746165a54ddc5a4defbf66383c9fb6",
+    "url": "/static/media/bank.1c746165.svg"
+  },
+  {
+    "revision": "6db68e8e9c523c6e578dd2e236874c2e",
+    "url": "/static/media/banner.6db68e8e.png"
+  },
+  {
+    "revision": "ef509e7a28de72799246b56a46b2b6e5",
+    "url": "/static/media/funds.ef509e7a.svg"
+  },
+  {
+    "revision": "cc604769268ab5948ded59b4000b27de",
+    "url": "/static/media/list.cc604769.svg"
+  },
+  {
+    "revision": "40a78ad1f7adfa3b6ab8fa4cf5c0dcf1",
+    "url": "/static/media/settings.40a78ad1.svg"
+  }
+]);
